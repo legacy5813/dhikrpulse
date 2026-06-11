a9b800fd22bb449d2ae5400b12ba4950503f999d
legacy5813 <hafiirahman200@gmail.com>
2026-06-11 17:41:37 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DhikrCounter />} />
           <Route path="/about" element={<About />} />
-        </Routes>
+        </Routes>   
       </div>
     </div>
   )
